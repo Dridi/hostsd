144ea37a7f530387cb1bbc584cb9a87e9d36e85b
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2014-03-18 20:33:45 +0100

    added a "nested" test suite
    This test suite simply makes sure hostsd will ignore nested directories
    inside the drop-in directory.

---
+++ tests/nested/tests.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Simple tests with a simple drop-in directory containing a nested directory.
+
+. "$PWD"/hostsd_functions.sh
+. "$PWD"/tests/helpers.sh
+
+test_find_files() {
+	local result_file=$(th_mktemp)
+	find_files "$TEST_DIR/hosts.d" >"$result_file"
+	th_assertSameFiles "Wrong file listing" "$TEST_DIR/files" "$result_file"
+}
+
+test_merge_files() {
+	local result_file=$(th_mktemp)
+	merge_files "$TEST_DIR/hosts.d" <"$TEST_DIR/files" >"$result_file"
+	th_assertSameFiles "Wrong hosts file" "$TEST_DIR/hosts" "$result_file"
+}
+
+test_create_hosts_file() {
+	local result_file=$(th_mktemp)
+	create_hosts_file "$TEST_DIR/hosts.d" "$result_file"
+	th_assertSameFiles "Wrong hosts file" "$TEST_DIR/hosts" "$result_file"
+}
+
+. /usr/share/shunit2/shunit2
